5d7e08540e3235f05a71cee1d0603f5d3784f429
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-11-14 08:30:31 -0500

    Changed Blink, Saved changes

---
+++ Blink/Blink.ino
@@ -0,0 +1,23 @@
+/*
+  Blink
+/*
+  Blink
+Programmer: Gavin Butcher
+Date: 11.14.2022
+Program: Blink
+  
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 12 as an output.
+  pinMode( 12, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
