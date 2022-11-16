387b6fdccbf77bbe66b4e1a0bd463c7054637393
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-11-16 07:41:24 -0500

    Initial AnalogReadSerial commit

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -10,8 +10,9 @@ Program: Blink
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 12 as an output.
+  // initialize digital pin 12 and 11 as an output.
   pinMode( 12, OUTPUT);
+  pinMode( 11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -19,5 +20,12 @@ void loop() {
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(2000);                       // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for a second
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
